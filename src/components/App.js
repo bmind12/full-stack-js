f5271e416db14a9c2f416734d427089fc710d482
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-14 16:17:41 +0100

    Change indentation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { Button } from antd;
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello world!
-      </div>
-    );
-  }
+class App extends PureComponent {
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        );
+    }
 }
 
 export default App;
