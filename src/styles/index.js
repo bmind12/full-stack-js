cd5c0c87b57b814c2d1611b15212aaaf9509ffce
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-17 10:56:39 +0100

    Remove a file copied by mistake

--- src/styles/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import input from '../reducers/input'
-
-export default createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
